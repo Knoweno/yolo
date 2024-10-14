f592286a84aa47bf51af38cfb9afa21166810a1c
Knowen.Obiso <knowenemmanuel@gmail.com>
2024-10-14 05:27:28 +0300

    Edit and confire API URL

--- client/src/api/apiConfig.js
+++ client/src/api/apiConfig.js
@@ -1,4 +1,5 @@
-// api/apiConfig.js
-const BASE_API_URL = 'http://localhost:5000/api/products';
+//const BASE_API_URL = 'http://localhost:5000/api/products';
+//
+const BASE_API_URL = 'http://35.193.80.100:5000/api/products';
 
 export default BASE_API_URL;
